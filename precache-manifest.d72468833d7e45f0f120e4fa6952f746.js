87a820ddc872ba1ebd61890e48e84a11e6c99d8b
Luke Russell <thelukerussel21@gmail.com>
2020-05-11 11:10:53 -0400

    Updates

---
+++ precache-manifest.d72468833d7e45f0f120e4fa6952f746.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4d5790825b4e78a267946e16d408cf",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "348dabcac5cd7db595ae",
+    "url": "/hangman/static/css/main.346e66da.chunk.css"
+  },
+  {
+    "revision": "b76f4a7bfa290718168f",
+    "url": "/hangman/static/js/2.60e57cf6.chunk.js"
+  },
+  {
+    "revision": "78b86c0095c74f94992db9795d80e258",
+    "url": "/hangman/static/js/2.60e57cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348dabcac5cd7db595ae",
+    "url": "/hangman/static/js/main.959dd0f3.chunk.js"
+  },
+  {
+    "revision": "0bebc4792bb334fb65e4",
+    "url": "/hangman/static/js/runtime-main.ee16e613.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman/static/media/6.c80394c2.jpg"
+  }
+]);
